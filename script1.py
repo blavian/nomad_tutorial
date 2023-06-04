cd19ba45129d2d69195a98554d6c7d5109d8d7ad
Benjamin Lavian <benylavian@gmail.com>
2023-06-04 12:39:08 -0400

    first commit

---
+++ script1.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+def one():
+    print (f'Script 1 has completed successfully at {datetime.now()}')
+
+one()
---
+++ script2.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+def two():
+    print (f'Script 2 has completed successfully at {datetime.now()}')
+
+two()
